be5758497b6a3c9ef31acd85151575c12f4b95a4
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-10 01:42:50 +0500

    Create ChatWeb.py

---
+++ ChatWeb.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route("/check")
+
+chat = 0
+
+num = 0
+
+num1 = 1
+
+def check():
+ while True:
+  global chat
+  global num
+  global num1
+  if num == 1:
+   num = 0
+   with open("chat.txt", "r") as file:
+    return str(file.read())
+  else:
+   num1 = 1
+   
+app.route("/<message>")
+
+while echo(message):
+ global chat
+ with open("chat.txt", "r") as file1:
+  chat = file1.read()
+ with open("chat.txt", "w") as file2:
+  file2.write(chat)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0" , port=8080)
