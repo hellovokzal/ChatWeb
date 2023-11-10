840fb2571fe4b3ea7216e209bf9da8d3bc5815e0
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-10 13:10:53 +0500

    Update ChatWeb.py

--- ChatWeb.py
+++ ChatWeb.py
@@ -15,21 +15,24 @@ def check():
   global chat
   global num
   global num1
-  if num == 1:
-   num = 0
-   with open("chat.txt", "r") as file:
-    return str(file.read())
-  else:
-   num1 = 1
+  while True:
+      if num == 1:
+      num = 0
+      with open("chat.txt", "r") as file:
+         return str(file.read())
+      else:
+          num1 = 1
    
 @app.route("/<message>")
 
 def echo(message):
  global chat
+ global num
  with open("chat.txt", "r") as file1:
   chat = file1.read()
  with open("chat.txt", "w") as file2:
   file2.write(chat)
+  num = 1
  return "Отправлено"
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port=8080)
