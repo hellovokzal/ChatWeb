8f4f9be1d3b24f02e10030c431e58e65a6343a24
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-10 15:13:02 +0500

    Update ChatWeb.py

--- ChatWeb.py
+++ ChatWeb.py
@@ -1,39 +1,36 @@
 from flask import Flask
 
 chat = ""
-
 num = 0
-
-mum1 = 1
+num1 = 1
 
 app = Flask(__name__)
 
 @app.route("/check")
-
 def check():
- while True:
-  global chat
-  global num
-  global num1
-  num = 0
-  while True:
-      if num == 1:
-         num = 0
-         with open("chat.txt", "r") as file:
-            return str(file.read())
-      else:
-         num1 = 1
-   
-@app.route("/<message>")
+    global chat
+    global num
+    global num1
+    num = 0
+    while True:
+        if num == 1:
+            num = 0
+            with open("chat.txt", "r") as file:
+                chat = file.read()
+            return chat
+        else:
+            num1 = 1
 
+@app.route("/<message>")
 def echo(message):
- global chat
- global num
- with open("chat.txt", "r") as file1:
-  chat = file1.read()
- with open("chat.txt", "w") as file2:
-  file2.write(chat)
-  num = 1
- return "Отправлено"
+    global chat
+    global num
+    with open("chat.txt", "r") as file1:
+        chat = file1.read()
+    with open("chat.txt", "w") as file2:
+        file2.write(chat)
+        num = 1
+    return "Отправлено"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0" , port=8080)
+    app.run(host="0.0.0.0", port=8080)
