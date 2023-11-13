f2ace5d8f8ecc437c18dcbaeac883d31f28b550e
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-14 03:13:57 +0500

    Update remoteweb1.py

--- remoteweb1.py
+++ remoteweb1.py
@@ -6,35 +6,9 @@ num = 0
 
 @app.route("/<path:message>")
 def echo(message):
-    global num
-    global num1
-    if message.startswith("https://"):
-        num = 9
-        num1 = 8
-        while True:
-            num = num + 1
-            text1 = message[:num]
-            if message[num1:num] == "/":
-                text1 = text1
-                break
-            num1 = num + 1
-    elif message.startswith("http://"):
-        num = 8
-        num1 = 7
-        while True:
-            num = num + 1
-            text1 = message[:num]
-            if message[num1:num] == "/":
-                text1 = text1
-                break
-            num1 = num + 1
     try:
-        if message.startswith("https://") or message.startswith("http://"):
-            url = get(message)
-            return str(url.text)
-        else:
-            url = get(f"{text1}{message}")
-            return str(url.text)
+        url = get(message)
+        return str(url.text)
     except:
         return "Ссылка не работает или не существует"
 
