729b63889078cd1ede9993c972d399f2fc0c49e1
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-11-16 04:44:32 +0500

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -5,7 +5,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Remote Terminal</title>
+</head>
+<body>
+    <form action="/command" method="post">
+        <input type="text" name="command" placeholder="Enter your command">
+        <input type="submit" value="Execute">
+    </form>
+    <div id="output"></div>
+    <script>
+        const form = document.querySelector('form');
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const formData = new FormData(form);
+            const response = await fetch('/command', {
+                method: 'POST',
+                body: formData
+            });
+            const result = await response.text();
+            const output = document.getElementById('output');
+            output.textContent = result;
+        });
+    </script>
+</body>
+</html>
+"""
 
 @app.route('/command', methods=['POST'])
 def execute_command():
